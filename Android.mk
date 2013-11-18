7710c9df859c61bc56be11afbb573d282863b56c
huangqiwu <huangqiwu@xiaomi.com>
2013-11-18 18:57:57 +0800

    audio : reconcile with CAF
    N/A
    
    Change-Id: I301a75b15a62a8a4e3c1a04fd9fd204a13b95d5b
    Signed-off-by: huangqiwu <huangqiwu@xiaomi.com>

--- Android.mk
+++ Android.mk
@@ -1,11 +1,11 @@
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 
-AUDIO_HW_ROOT := $(call my-dir)
+MY_LOCAL_PATH := $(call my-dir)
 
-ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
-    include $(AUDIO_HW_ROOT)/alsa_sound/Android.mk
-    include $(AUDIO_HW_ROOT)/libalsa-intf/Android.mk
-    include $(AUDIO_HW_ROOT)/audiod/Android.mk
-    include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
+ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
+include $(MY_LOCAL_PATH)/legacy/Android.mk
+else
+include $(MY_LOCAL_PATH)/hal/Android.mk
 endif
+
 endif
