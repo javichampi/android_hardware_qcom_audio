c8548cc9254c0b763f0eff9be4bd2fb02a494e77
huangqiwu <huangqiwu@xiaomi.com>
2014-01-24 10:55:18 +0800

    Revert "Revert "audio: Fix for AV Sync related issues""
    This reverts commit a4dd6fe0c7b068ef0d7a68c2164a7deddc599b84.
    
    Change-Id: Ib377ca1eb889986be79f839a713155a5b87d03ae

--- legacy/alsa_sound/AudioHardwareALSA.cpp
+++ legacy/alsa_sound/AudioHardwareALSA.cpp
@@ -3845,7 +3845,7 @@ void AudioHardwareALSA::extOutThreadFunc() {
     }
 
     pid_t tid  = gettid();
-    androidSetThreadPriority(tid, ANDROID_PRIORITY_AUDIO);
+    androidSetThreadPriority(tid, ANDROID_PRIORITY_URGENT_AUDIO);
     prctl(PR_SET_NAME, (unsigned long)"ExtOutThread", 0, 0, 0);
 
     int ionBufCount = 0;
@@ -3854,7 +3854,7 @@ void AudioHardwareALSA::extOutThreadFunc() {
     uint32_t bytesAvailInBuffer = 0;
     uint32_t proxyBufferTime = 0;
     void  *data;
-    int err = NO_ERROR;
+    status_t err = NO_ERROR;
     ssize_t size = 0;
     void * outbuffer= malloc(AFE_PROXY_PERIOD_SIZE);
 
@@ -3884,7 +3884,12 @@ void AudioHardwareALSA::extOutThreadFunc() {
             }
         }
         err = mALSADevice->readFromProxy(&data, &size);
-        if (err < 0) {
+        if(err == (status_t) FAILED_TRANSACTION) {
+            ALOGE("readFromProxy returned an error, mostly a flush or an under run continuing");
+            err = NO_ERROR;
+            continue;
+        }
+        if(err < 0  || size <= 0) {
             ALOGE("ALSADevice readFromProxy returned err = %d,data = %p,\
                     size = %d", err, data, size);
             continue;
